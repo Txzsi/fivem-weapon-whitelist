1ced91c5bbd0e29602c1b94fd22ff985b8c4883a
Txzsi <81436380+Txzsi@users.noreply.github.com>
2022-12-19 03:58:02 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ wepcfg.bypass_role = ""
 wepcfg.error_msg = "^1Error^0: You do not have the correct permissions to use this weapon!"
 
 wepcfg.whitelisted_weapons = {
-    ["123"] = {
+    ["123"] = { -- role id
         {WepName = "WEAPON_PISTOL" }
     },
     ["313"] = {
